24bb9d82d2e5b06ba730155f8744ed13ac805356
Ilya Hancharuk <innot707@gmail.com>
2022-07-01 20:50:56 +0300

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n)
+    str[0] ==='-' ? str = str.slice(1) : str = str
+    let result = ''
+    for (i = 0; i < str.length; i++) {
+        result = `${str[i]}${result}`
+    }
+    return +result
 }
